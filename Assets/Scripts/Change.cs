0e5a51cf56895d0c8a2df6dd6b1849d2f588f10f
vietdung123bg <sonthanh123bg@gmail.com>
2022-06-21 10:59:16 +0700

    Create new game unity project

---
+++ Assets/Scripts/Change.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Change : MonoBehaviour
+{
+public void Play(){
+    SceneManager.LoadScene("SampleScene");
+}
+}
